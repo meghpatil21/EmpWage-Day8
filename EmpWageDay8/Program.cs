aa7a9f2f4ef30fbfc876ec3685c0cd299a3b7bd9
meghpatil21 <megh.after18@gmail.com>
2022-11-01 00:01:08 +0530

    Implemented UC6

--- EmpWageDay8/Program.cs
+++ EmpWageDay8/Program.cs
@@ -4,28 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            Console.WriteLine();
-
-            Console.WriteLine("Select Option\n1.Employee Attendace \n2.Daily Wage \n3.Part Time Wage \n4.Switch Case \n5.Monthly Wage");
-            int opt =Convert.ToInt32(Console.ReadLine());
-
-            switch(opt)
+           int num = 0;
+           while(num ==0)
             {
-                case 1: PresentAbsent.empCheck();
-                    break;
-                case 2: DailyWage.dailyWge();
-                    break;
-                case 3: PartTimeWage.prtTimeWge();
-                    break;
-                case 4: SwitchCase.Switch();
-                    break;
-                case 5: WageForMonth.monthlyWage();
-                    break;
+                Console.WriteLine();
+                Console.WriteLine("**************************************");
+                Console.WriteLine("Welcome to Employee Wage Computation");
+                Console.WriteLine();
 
-                default: Console.WriteLine("Invaild Choice");
-                    break;
+                Console.WriteLine("Select Option\n1.Employee Attendace \n2.Daily Wage \n3.Part Time Wage \n4.Switch Case \n5.Monthly Wage \n6.100 Hours OR 20 Days \n7.Exit");
+                int opt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (opt)
+                {
+                    case 1:
+                        PresentAbsent.empCheck();
+                        break;
+                    case 2:
+                        DailyWage.dailyWge();
+                        break;
+                    case 3:
+                        PartTimeWage.prtTimeWge();
+                        break;
+                    case 4:
+                        SwitchCase.Switch();
+                        break;
+                    case 5:
+                        WageForMonth.monthlyWage();
+                        break;
+                    case 6:
+                        WorkingHoursCondn.wrkingHrs();
+                        break;
+                    case 7: num++;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invaild Choice");
+                        break;
+                }
             }
+           
+
         }
     }
 }
