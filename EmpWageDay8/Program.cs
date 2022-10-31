dbc290dc10d08f375a56e25f603d964230fe01a9
meghpatil21 <megh.after18@gmail.com>
2022-10-31 23:13:01 +0530

    Welcome Msg

---
+++ EmpWageDay8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageDay8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
