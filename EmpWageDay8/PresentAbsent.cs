5a0ba4f2d16233c94617905001b85d76b9119c7f
meghpatil21 <megh.after18@gmail.com>
2022-10-31 23:24:25 +0530

    Implemented uc1

---
+++ EmpWageDay8/PresentAbsent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageDay8
+{
+    internal class PresentAbsent
+    {
+        public static void empCheck()
+        {
+            Random random = new Random();
+            int emp=random.Next(0,2);
+            if (emp==0)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+
+        }
+    }
+}
--- EmpWageDay8/Program.cs
+++ EmpWageDay8/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            Console.WriteLine();
+
+            Console.WriteLine("Select Option\n1.Employee Attendace");
+            int opt =Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1: PresentAbsent.empCheck();
+                    break;
+
+                default: Console.WriteLine("Invaild Choice");
+                    break;
+            }
         }
     }
 }
